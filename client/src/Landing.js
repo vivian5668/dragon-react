539cdf2af8471a9f176e3e084d87ae8aab172082
Scott Molsness <smolsness@gmail.com>
2018-03-16 08:44:48 -0700

    gallery, modals, about, team, fonts updated

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -6,7 +6,7 @@ import React from 'react';
         <div className="container">
           <div className="intro-text">
             <div className="intro-heading text-uppercase">Welcome to Dragons 'R Us</div>
-            <div className="intro-lead-in">The online resoure for purchasing and styling your fire-breathing pets</div>
+            <div className="intro-lead-in">The online resoure for your fire-breathing pets</div>
             {props.signupButton}
             <button className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" id='landingButton1' href="#services">See What We Do</button>
             {props.loginButton}
