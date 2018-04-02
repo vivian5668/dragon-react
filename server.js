bbe9cf1548b93d006926e545eb76279648427915
Chelsea Zhu <qianwei5668@gmail.com>
2018-04-02 13:34:41 -0700

    deployment

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser');
 
 // Mongoose stuff
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mernJwtAuth');
+// mongoose.connect('mongodb://localhost/mernJwtAuth');
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 var auth = require('./routes/auth');
 
@@ -15,7 +16,8 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
 // Do we still need this?
 app.use(function(req, res, next) {
@@ -25,6 +27,10 @@ app.use(function(req, res, next) {
 });
 
 app.use('/auth', auth);
+
+app.get('*', function(req, res, next) {
+	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
